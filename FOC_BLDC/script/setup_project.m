511b1f4c980d89583f8457ca05edb3b12f48fe1b
qiany <1246716773@qq.com>
2026-03-31 23:03:20 +0800

    更新simulink工程描述

--- FOC_BLDC/script/setup_project.m
+++ FOC_BLDC/script/setup_project.m
@@ -1,7 +1,7 @@
 clear;
 clc;
-disp('本工程文件仅适用于EHB控制系统');
-disp('若有任何问题请及时联系ECU科室');
+disp('本工程文件仅适用于FOC控制');
+disp('若有任何问题请及时联系qianye');
 % calin=[-9.96 -7.98 -5.98 -3.99 -1.994 0 1.995 3.992 5.99 7.98 9.97];
 % calout=[-10 -8 -6 -4 -2 0 2 4 6 8 10];
 % uiopen('D:\Matlab\QianXun_Prj\mdl\QIANXUN.sldd',1);
